f410eca2d787e70617cb98f0fe618dbaa5a1e2c0
prathimabhat <prathimabhatm01@gmail.com>
2020-11-18 13:32:19 +0530

    comments model added

--- community_forum/models.py
+++ community_forum/models.py
@@ -51,3 +51,16 @@ class Answers(models.Model):
 		return f"{self.id}"
 
 
+class Comments(models.Model):
+	class Meta:
+		verbose_name_plural="Comments"
+
+	id=models.AutoField(primary_key=True,unique=True)
+	user=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='Comments',blank=True,null=True)
+	question=models.ForeignKey(Questions,on_delete=models.CASCADE,related_name='Comments',blank=True,null=True)
+	answer=models.ForeignKey(Answers,on_delete=models.CASCADE,related_name='Comments',blank=True,null=True)
+	comment=RichTextField(blank=True,null=True)
+	date=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.id}"
